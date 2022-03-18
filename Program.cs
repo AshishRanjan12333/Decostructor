0625173f19625715c63b823b5167977b6a481de5
A R <A R@LAPTOP-M0TJ75U0>
2022-03-18 14:07:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decostructor
+{
+    public class Destructor
+    {
+        public Destructor()
+        {
+            Console.WriteLine("object is created");
+        }
+        ~Destructor()
+        {
+            Console.WriteLine("obect is destroyed");
+        }
+        static void Main(String[] args)
+        {
+            Destructor ob = new Destructor();
+            ob = null;
+            GC.Collect();   //call destructor explicitly
+            Console.ReadLine();
+        }
+    } 
+    }
